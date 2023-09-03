f8b544ba9d883dad5247ce9a9db729687000b03d
이예리 <leeyr0412@naver.com>
2023-09-03 23:14:52 +0900

    feat: History Entity 생성 (#17)

--- server/src/main/java/com/ssafy/completionism/domain/transaction/History.java
+++ server/src/main/java/com/ssafy/completionism/domain/transaction/History.java
@@ -1,10 +1,15 @@
 package com.ssafy.completionism.domain.transaction;
 
 import com.ssafy.completionism.domain.TimeBaseEntity;
+import com.ssafy.completionism.domain.member.Member;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+
+import java.time.LocalDateTime;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -17,4 +22,37 @@ public class History extends TimeBaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "history_id")
     private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int income;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int outcome;
+
+    @Column(nullable = false)
+    @Lob
+    private String diary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    private History(Long id, LocalDateTime date, String type, int income, int outcome, String diary, Member member) {
+        this.id = id;
+        this.date = date;
+        this.type = type;
+        this.income = income;
+        this.outcome = outcome;
+        this.diary = diary;
+        this.member = member;
+    }
 }
