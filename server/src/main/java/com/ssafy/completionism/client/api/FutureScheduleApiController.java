a337e50b931c131811e18a3e0c72da63d4e8760c
이진하 <ljh8190@naver.com>
2023-09-03 23:15:43 +0900

    feat: 미래 예상 소비 등록 skeleton controller 작성

--- server/src/main/java/com/ssafy/completionism/client/api/FutureScheduleApiController.java
+++ server/src/main/java/com/ssafy/completionism/client/api/FutureScheduleApiController.java
@@ -1,9 +1,13 @@
 package com.ssafy.completionism.client.api;
 
+import com.ssafy.completionism.client.api.request.CreateFutureScheduleRequest;
 import com.ssafy.completionism.client.schedule.service.FutureScheduleService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +20,9 @@ public class FutureScheduleApiController {
 
     private final FutureScheduleService futureScheduleService;
 
+    @ApiOperation(value = "미래 예상 소비 등록")
+    @PostMapping
+    public ApiResult<Void> createFutureSchedule(@RequestBody CreateFutureScheduleRequest request) {
+        return null;
+    }
 }
