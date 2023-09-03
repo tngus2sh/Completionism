ca321621ec9a7fe187d558d906e48a1d9d431987
이진하 <ljh8190@naver.com>
2023-09-03 23:22:02 +0900

    feat: Schedule 테이블 생성

--- server/src/main/java/com/ssafy/completionism/client/schedule/Schedule.java
+++ server/src/main/java/com/ssafy/completionism/client/schedule/Schedule.java
@@ -1,11 +1,13 @@
 package com.ssafy.completionism.client.schedule;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,4 +17,40 @@ public class Schedule {
     @GeneratedValue
     @Column(name = "schedule_id")
     Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false)
+    private String todo;
+
+    @Column(nullable = false)
+    private int cost;
+
+    @Column(nullable = false)
+    private boolean plus;
+
+    @Column(nullable = false)
+    private boolean fixed;
+
+    @Column(name = "period_type")
+    private boolean periodType;
+
+    @Column
+    private int period;
+
+
+    public Schedule() {}
+
+    @Builder
+    public Schedule(Long id, LocalDateTime date, String todo, int cost, boolean plus, boolean fixed, boolean periodType, int period) {
+        this.id = id;
+        this.date = date;
+        this.todo = todo;
+        this.cost = cost;
+        this.plus = plus;
+        this.fixed = fixed;
+        this.periodType = periodType;
+        this.period = period;
+    }
 }
