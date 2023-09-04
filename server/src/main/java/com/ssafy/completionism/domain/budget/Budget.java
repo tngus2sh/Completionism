9dfed1b523fff6e1b2c703ff80fd61e8d102792b
tngus2sh <ssuhyeon98@gmail.com>
2023-09-04 23:45:20 +0900

    refactor: member 연관성 넣기 (#4)

--- server/src/main/java/com/ssafy/completionism/domain/budget/Budget.java
+++ server/src/main/java/com/ssafy/completionism/domain/budget/Budget.java
@@ -2,12 +2,13 @@ package com.ssafy.completionism.domain.budget;
 
 import com.ssafy.completionism.domain.Category;
 import com.ssafy.completionism.domain.TimeBaseEntity;
+import com.ssafy.completionism.domain.member.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -19,22 +20,23 @@ public class Budget extends TimeBaseEntity {
     @Column(name = "budget_id")
     private Long id;
 
-//    @Column(name = "member_id")
-//    @ManyToOne
-//    private Member member;
+    @Column(name = "member_id")
+    @ManyToOne
+    private Member member;
 
     @Column(name = "year_month")
-    private Date yearMonth;
+    private LocalDate yearMonth;
 
     @Column(name = "total_budget")
     private int totalBudget;
 
-    @Column(name = "category")
+    @Column
     private Category category;
 
     @Builder
-    public Budget(Long id, Date yearMonth, int totalBudget, Category category) {
+    public Budget(Long id, Member member, LocalDate yearMonth, int totalBudget, Category category) {
         this.id = id;
+        this.member = member;
         this.yearMonth = yearMonth;
         this.totalBudget = totalBudget;
         this.category = category;
