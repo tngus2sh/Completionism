a5fb897515fd0c77fbb8095c9708445d82769ecf
tngus2sh <ssuhyeon98@gmail.com>
2023-09-08 22:18:42 +0900

    refactor: 파리미터 사용자 아이디로 변경 및 목표 예산 수정 코드 추가 (#5)

--- server/src/main/java/com/ssafy/completionism/api/service/budget/impl/BudgetServiceImpl.java
+++ server/src/main/java/com/ssafy/completionism/api/service/budget/impl/BudgetServiceImpl.java
@@ -1,24 +1,21 @@
 package com.ssafy.completionism.api.service.budget.impl;
 
 import com.ssafy.completionism.api.controller.budget.response.MonthBudgetResponse;
-import com.ssafy.completionism.api.exception.NotFoundException;
 import com.ssafy.completionism.api.service.budget.BudgetService;
 import com.ssafy.completionism.api.service.budget.dto.AddBudgetDto;
 import com.ssafy.completionism.api.service.budget.dto.ModifyBudgetDto;
-import com.ssafy.completionism.domain.Category;
 import com.ssafy.completionism.domain.budget.Budget;
 import com.ssafy.completionism.domain.budget.repository.BudgetQueryRepository;
 import com.ssafy.completionism.domain.budget.repository.BudgetRepository;
 import com.ssafy.completionism.domain.member.Member;
+import com.ssafy.completionism.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,19 +25,18 @@ public class BudgetServiceImpl implements BudgetService {
     private final BudgetRepository budgetRepository;
     private final BudgetQueryRepository budgetQueryRepository;
 
-//    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 예산 등록
-     * @param phone 사용자 전화번호
+     * @param loginId 사용자 전화번호
      * @param dto 예산 등록 dto
      * @return 예산 식별키
      */
     @Override
-    public Long addBudget(String phone, AddBudgetDto dto) {
+    public Long addBudget(String loginId, AddBudgetDto dto) {
 
-//        Member member = memberRepository.findByPhone(phone).orElseThrow(NoSuchElementException::new);
-        Member member = null;
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(NoSuchElementException::new);
 
         Budget budget = Budget.toBudget(member, dto.getYearMonth(), dto.getTotalBudget(), dto.getCategory());
         Budget savedBudget = budgetRepository.save(budget);
@@ -49,45 +45,44 @@ public class BudgetServiceImpl implements BudgetService {
 
     /**
      * 해당 사용자의 매달 예산 비용 리스트
-     * @param phone 사용자 전화번호
+     * @param loginId 사용자 전화번호
      * @return 예산 비용 리스트
      */
     @Override
-    public List<MonthBudgetResponse> searchMonthAll(String phone) {
-//        Member member = memberRepository.findByPhone(phone).orElseThrow(NoSuchElementException::new);
-        Member member = null;
+    public List<MonthBudgetResponse> searchMonthAll(String loginId) {
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(NoSuchElementException::new);
 
         return budgetQueryRepository.getMonthBudgetAll(member.getId());
     }
 
     /**
      * 해당 사용자의 해당 기간 매달 예산 비용 리스트
-     * @param phone 사용자 전화번호
+     * @param loginId 사용자 전화번호
      * @param startMonth 기간 시작월
      * @param endMonth 기간 끝월
      * @return 해당 사용자의 해당 기간 매달 예산 비용 리스트
      */
     @Override
-    public List<MonthBudgetResponse> searchMonth(String phone, LocalDate startMonth, LocalDate endMonth) {
-//        Member member = memberRepository.findByPhone(phone).orElseThrow(NoSuchElementException::new);
-        Member member = null;
+    public List<MonthBudgetResponse> searchMonth(String loginId, LocalDate startMonth, LocalDate endMonth) {
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(NoSuchElementException::new);
 
         return budgetQueryRepository.getMonthBudget(member.getId(), startMonth, endMonth);
     }
 
     /**
      * 해당 사용자의 예산 수정
-     * @param phone 사용자 전화번호
+     * @param loginId 사용자 전화번호
      * @param dto 수정할 예산 정보
-     * @return 예산 식별키
      */
     @Override
-    public Long modifyBudget(String phone, ModifyBudgetDto dto) {
+    public void modifyBudget(String loginId, ModifyBudgetDto dto) {
 
-//        Member member = memberRepository.findByPhone(phone).orElseThrow(NoSuchElementException::new);
-        Member member = null;
+        Member member = memberRepository.findByLoginId(loginId).orElseThrow(NoSuchElementException::new);
 
-        // TODO: 에산 정보 수정
-        return null;
+        // 목표 예산 수정
+        budgetQueryRepository.findByYearMonthAndMember(dto.getYearMonth(), member)
+                .ifPresent((budget) -> {
+                    budget.updateBudget(dto.getTotalBudget(), dto.getCategory());
+        });
     }
 }
