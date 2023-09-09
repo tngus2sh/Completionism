7013b76913a90eb2e8c643b33e22375586a11093
tngus2sh <ssuhyeon98@gmail.com>
2023-09-09 23:57:35 +0900

    feat: 다이어리 컨트롤러 작성  (#28)

---
+++ server/src/main/java/com/ssafy/completionism/api/controller/diary/DiaryApiController.java
@@ -0,0 +1,19 @@
+package com.ssafy.completionism.api.controller.diary;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/diary")
+public class DiaryApiController {
+
+
+    @GetMapping
+    public void getDiarys() {
+    }
+}
