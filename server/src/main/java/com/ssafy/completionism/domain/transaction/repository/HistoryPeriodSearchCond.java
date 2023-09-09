14a00aa70eb360753a9ccaa6fda793c563e0426a
이예리 <leeyr0412@naver.com>
2023-09-10 00:14:52 +0900

    feat: 하루거래내역 검색 조건 구현 (#19)

--- server/src/main/java/com/ssafy/completionism/domain/transaction/repository/HistoryPeriodSearchCond.java
+++ server/src/main/java/com/ssafy/completionism/domain/transaction/repository/HistoryPeriodSearchCond.java
@@ -1,4 +1,20 @@
 package com.ssafy.completionism.domain.transaction.repository;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
 public class HistoryPeriodSearchCond {
+
+    private LocalDateTime startDay;
+    private LocalDateTime endDay;
+
+    @Builder
+    public HistoryPeriodSearchCond(LocalDate startDay, LocalDate endDay) {
+        this.startDay = startDay.atStartOfDay();
+        this.endDay = endDay.plusDays(1).atStartOfDay();
+    }
 }
