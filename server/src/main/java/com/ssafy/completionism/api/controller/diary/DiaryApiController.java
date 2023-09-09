24a63b28abcda813bebcc3ddffb0b2d8962a471b
tngus2sh <ssuhyeon98@gmail.com>
2023-09-10 00:12:39 +0900

    feat: 다이어리 추가, 전체 조회, 상세 조회 구조 작성  (#28)

--- server/src/main/java/com/ssafy/completionism/api/controller/diary/DiaryApiController.java
+++ server/src/main/java/com/ssafy/completionism/api/controller/diary/DiaryApiController.java
@@ -1,11 +1,14 @@
 package com.ssafy.completionism.api.controller.diary;
 
+import com.ssafy.completionism.api.controller.diary.request.AddDiaryRequest;
+import com.ssafy.completionism.api.controller.diary.response.DiaryResponse;
+import com.ssafy.completionism.global.utils.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -13,14 +16,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/diary")
 public class DiaryApiController {
 
+    @PostMapping
+    public void addDiary(
+            @Valid @RequestBody AddDiaryRequest request
+    ) {
+        // TODO: 2023/09/10  다이어리 추가 서비스
+    }
+
 
     @GetMapping
-    public void getDiarys() {
+    public List<DiaryResponse> showDiarys() {
+        // 사용자 정보 가져오기
+        String loginId = SecurityUtils.getCurrentLoginId();
+
+        // TODO: 2023/09/10 전체 다이어리 보여주기
+
+        return null;
     }
 
     @GetMapping("/{diary_id}")
-    public void getDiary(@PathVariable(name = "diary_id") Long diaryId) {
+    public DiaryResponse showDiary(
+            @PathVariable(name = "diary_id") Long diaryId
+    ) {
+        // 사용자 정보 가져오기
+        String loginId = SecurityUtils.getCurrentLoginId();
 
+        // TODO: 2023/09/10 세부 다이어리 보여주기
+
+        return null;
     }
 
 }
