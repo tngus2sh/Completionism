dc55e063bed56b8cddfb15021dfa1d3c70499028
tngus2sh <ssuhyeon98@gmail.com>
2023-09-09 23:59:14 +0900

    feat: 다이어리 컨트롤러 다이어리 세부 조회 추가  (#28)

--- server/src/main/java/com/ssafy/completionism/api/controller/diary/DiaryApiController.java
+++ server/src/main/java/com/ssafy/completionism/api/controller/diary/DiaryApiController.java
@@ -3,6 +3,7 @@ package com.ssafy.completionism.api.controller.diary;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +17,10 @@ public class DiaryApiController {
     @GetMapping
     public void getDiarys() {
     }
+
+    @GetMapping("/{diary_id}")
+    public void getDiary(@PathVariable(name = "diary_id") Long diaryId) {
+
+    }
+
 }
