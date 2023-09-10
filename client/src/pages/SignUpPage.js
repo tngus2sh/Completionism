2fe30632c08691a507f687510ae970be036d1f75
오정원 <wjddnjs0802@ajou.ac.kr>
2023-09-10 19:02:59 +0900

    chore: axios 요청 라이브러리 설치  (#3)

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,22 +1,94 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Grid, TextField, Button } from "@mui/material";
+import axios from "axios";
 import UpperNavigationBar from "../components/UpperNavigationBar";
 
+const SignUpPage = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    name: "",
+    email: "",
+  });
 
-const SignUpPage = ()=> {
-    const upperNavbarName = "회원가입";
+  const upperNavbarName = "회원가입";
 
-    return(
-        <Grid>
-            <Grid>
-                <UpperNavigationBar props = {upperNavbarName}/>
-            </Grid>
-            <Grid className="progressive_bar">
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-            </Grid>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        </Grid>
-    )
-}
+    try {
+      // 회원가입 요청을 보내기 전에 입력 데이터를 확인하고 유효성 검사를 수행할 수 있습니다.
+      // 이 예제에서는 간단한 데이터 유효성 검사만 수행합니다.
+      if (!formData.username || !formData.password || !formData.name || !formData.email) {
+        alert("모든 필드를 채워주세요.");
+        return;
+      }
 
-export default SignUpPage
+      // Axios를 사용하여 회원가입 요청을 보냅니다.
+      const response = await axios.post("/api/signup", formData);
+
+      // 회원가입 성공 시 처리할 코드를 여기에 추가할 수 있습니다.
+
+      // 예를 들어, 회원가입 성공 메시지를 출력하거나 다른 페이지로 이동할 수 있습니다.
+
+      alert("회원가입이 성공적으로 완료되었습니다.");
+    } catch (error) {
+      // 회원가입 실패 시 처리할 코드를 여기에 추가할 수 있습니다.
+      alert("회원가입 중 오류가 발생했습니다.");
+    }
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <UpperNavigationBar props={upperNavbarName} />
+      </Grid>
+      <Grid item xs={12} className="progressive_bar">
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="아이디"
+            name="username"
+            fullWidth
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <TextField
+            label="비밀번호"
+            name="password"
+            type="password"
+            fullWidth
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <TextField
+            label="이름"
+            name="name"
+            fullWidth
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <TextField
+            label="이메일"
+            name="email"
+            fullWidth
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            회원가입
+          </Button>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SignUpPage;
