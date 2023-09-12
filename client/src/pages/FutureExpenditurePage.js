e4d86b033ac448652ef7ce5683589a5b05ba75de
오정원 <wjddnjs0802@ajou.ac.kr>
2023-09-13 00:07:18 +0900

    chore: 가상키패드 라이브러리 설치 (#3)

--- client/src/pages/FutureExpenditurePage.js
+++ client/src/pages/FutureExpenditurePage.js
@@ -1,15 +1,34 @@
 import { Grid } from '@mui/material';
-import React from "react";
+import React, { useState } from "react";
 import UnderNavigationBar from "../components/UnderNavigationBar";
 import UpperNavigationBar from "../components/UpperNavigationBar";
 import "./FutureExpenditurePage.css";
 import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useEffect } from 'react';
+import axios from 'axios';
+import SampleKeyboard from '../components/virtualKeyboard';
 
-const FutureExpenditurePage = ()=>{
+const FutureExpenditurePage = () => {
     const selectedYearAndMonth = useSelector((state) => state.auth.selectedYearAndMonth);
     const upperNavbarName = `${selectedYearAndMonth} 미래소비`;
 
-    return(
+    const [isKeyboardVisible, setKeyboardVisible] = useState(false); // 키보드 가시성 상태
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    const loadData = async () => {
+        // ...
+
+    };
+
+    // 키보드 토글 함수
+    const toggleKeyboard = () => {
+        setKeyboardVisible(!isKeyboardVisible);
+    };
+
+    return (
         <Grid>
             <Grid className="uppernavbar">
                 <UpperNavigationBar props={upperNavbarName}/>
@@ -20,12 +39,22 @@ const FutureExpenditurePage = ()=>{
             <Grid className="body">
                 this is FutureExpenditurePage
             </Grid>
-            
+
+            {/* 가상 쿼티 키보드 열기 */}
+            <Grid>
+                <button onClick={toggleKeyboard}>미래소비 생성하기</button>
+            </Grid>
+
+            {/* 키보드 가시성에 따라 렌더링 */}
+            {isKeyboardVisible && (
+                <SampleKeyboard />
+            )}
+
             <Grid className="undernavbar">
                 <UnderNavigationBar/>
             </Grid>
         </Grid>
-    )
+    );
 }
 
-export default FutureExpenditurePage
+export default FutureExpenditurePage;
