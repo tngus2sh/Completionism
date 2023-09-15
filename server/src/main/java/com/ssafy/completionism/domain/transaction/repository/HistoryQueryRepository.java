b19c65960651a72d193f821ac7e35d420556f921
이예리 <leeyr0412@naver.com>
2023-09-15 22:20:15 +0900

    feat: 날짜 거래내역 조회 쿼리 작성

--- server/src/main/java/com/ssafy/completionism/domain/transaction/repository/HistoryQueryRepository.java
+++ server/src/main/java/com/ssafy/completionism/domain/transaction/repository/HistoryQueryRepository.java
@@ -3,16 +3,21 @@ package com.ssafy.completionism.domain.transaction.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.completionism.api.controller.transaction.response.DiaryResponse;
 import com.ssafy.completionism.api.controller.transaction.response.HistoryResponse;
+import com.ssafy.completionism.api.controller.transaction.response.TransactionResponse;
 import com.ssafy.completionism.domain.transaction.History;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static com.querydsl.core.types.Projections.constructor;
 import static com.ssafy.completionism.domain.transaction.QHistory.history;
+import static com.ssafy.completionism.domain.transaction.QTransaction.transaction;
 
 @Repository
 public class HistoryQueryRepository {
@@ -21,6 +26,34 @@ public class HistoryQueryRepository {
 
     public HistoryQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<TransactionResponse> getTransactionList(String loginId, LocalDate date) {
+
+        List<Long> ids = queryFactory.select(transaction.id)
+                .from(transaction)
+                .join(transaction.history, history)
+                .where(history.member.loginId.eq(loginId),
+                        history.createdDate.between(date.atStartOfDay(), date.plusDays(1).atStartOfDay()))
+                .orderBy(transaction.createdDate.desc())
+                .fetch();
+
+        if (CollectionUtils.isEmpty(ids)) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory.select(constructor(TransactionResponse.class,
+                        transaction.id,
+                        transaction.time,
+                        transaction.cost,
+                        transaction.plus,
+                        transaction.category,
+                        transaction.place,
+                        transaction.diary,
+                        transaction.feel))
+                .from(transaction)
+                .where(transaction.id.in(ids))
+                .fetch();
     }
 
     public List<HistoryResponse> getHistoryResponseForPeriod(String loginId, HistoryPeriodSearchCond cond) {
@@ -56,6 +89,7 @@ public class HistoryQueryRepository {
                 .orderBy(history.createdDate.asc())
                 .fetch();
     }
+
     public Optional<DiaryResponse> getRegisteredDiary(String loginId, LocalDateTime transactionTime) {
         return Optional.ofNullable(queryFactory.select(constructor(DiaryResponse.class,
                         history.id,
