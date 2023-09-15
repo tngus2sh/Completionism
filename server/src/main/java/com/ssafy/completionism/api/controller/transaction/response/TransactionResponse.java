f88ef1b5252e86b7ac022d54bcd525039508befa
이예리 <leeyr0412@naver.com>
2023-09-15 21:31:47 +0900

    refactor: 거래내역 반환값 변경

--- server/src/main/java/com/ssafy/completionism/api/controller/transaction/response/TransactionResponse.java
+++ server/src/main/java/com/ssafy/completionism/api/controller/transaction/response/TransactionResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.completionism.api.controller.transaction.response;
 
+import com.ssafy.completionism.domain.transaction.Feel;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,10 +14,10 @@ public class TransactionResponse {
     private String category;
     private String place;
     private String diary;
-    private String feel;
+    private Feel feel;
 
     @Builder
-    public TransactionResponse(Long id, String time, int cost, boolean isPlus, String category, String place, String diary, String feel) {
+    public TransactionResponse(Long id, String time, int cost, boolean isPlus, String category, String place, String diary, Feel feel) {
         this.transactionId = id;
         this.time = time;
         this.cost = cost;
