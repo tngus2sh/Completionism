99a82cfeb2cd0be13ecf488ee8dae8e2969fbc4e
tngus2sh <ssuhyeon98@gmail.com>
2023-09-16 20:17:29 +0900

    feat: 일기 작성 페이지

--- client/src/pages/DiaryPage.js
+++ client/src/pages/DiaryPage.js
@@ -4,30 +4,40 @@ import UnderNavigationBar from "../components/UnderNavigationBar";
 import UpperNavigationBar from "../components/UpperNavigationBar";
 import "./AccountBookPage.css";
 import { CalenderForDiary } from "../components/CalendarForDiary";
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const DiaryPage = ()=>{
-    const upperNavbarName = "일기"
+const DiaryPage = () => {
+  const upperNavbarName = "일기"
 
-    return(
-        <div>
-            <div className="uppernavbar">
-                <UpperNavigationBar props={upperNavbarName}/>
-            </div>
+  return (
+    <div>
+      <div className="uppernavbar">
+        <UpperNavigationBar props={upperNavbarName} />
+      </div>
 
 
-            <div className="body">
-                this is DiaryPage
-            </div>
-            
-            <div>
-                <CalenderForDiary/>
-            </div>
+      <div className="body">
+        this is DiaryPage
+      </div>
 
-            <div className="undernavbar">
-                <UnderNavigationBar/>
-            </div>
+      <div>
+        <CalenderForDiary />
+      </div>
+
+      <div>
+        <div className="diary-button">
+          <Link to="/diary/write" className="diary-link">
+            일기 쓰러 가자!!
+          </Link>
         </div>
-    )
+      </div>
+
+      <div className="undernavbar">
+        <UnderNavigationBar />
+      </div>
+    </div>
+  )
 }
 
 export default DiaryPage
