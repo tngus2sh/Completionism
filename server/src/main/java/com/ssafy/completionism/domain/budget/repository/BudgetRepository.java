6eb2d6accdf892fa9442c8c4735816a23debaef3
tngus2sh <ssuhyeon98@gmail.com>
2023-09-16 16:03:42 +0900

    refactor: 예산 수정 중복 출력 수정

--- server/src/main/java/com/ssafy/completionism/domain/budget/repository/BudgetRepository.java
+++ server/src/main/java/com/ssafy/completionism/domain/budget/repository/BudgetRepository.java
@@ -1,13 +1,9 @@
 package com.ssafy.completionism.domain.budget.repository;
 
-import com.ssafy.completionism.domain.Category;
 import com.ssafy.completionism.domain.budget.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
